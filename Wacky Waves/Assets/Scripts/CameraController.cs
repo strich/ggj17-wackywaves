0edc1b6880baf79ecc96a09192c5f3d07565bece
Scott Richmond <s.t.richmond@gmail.com>
2017-01-21 10:49:20 +0000

    Main Camera prefabs and now follows the Player.

---
+++ Wacky Waves/Assets/Scripts/CameraController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	private GameObject _player;
+	public float RotateFollowSmoothing = 1.0f;
+
+	void Start ()
+	{
+		_player = GameObject.Find("Player");
+
+		transform.position = new Vector3(
+			_player.transform.position.x, 
+			_player.transform.position.y + 10, 
+			_player.transform.position.z);
+
+		transform.rotation = Quaternion.Euler(
+			90, 
+			_player.transform.rotation.y, 
+			_player.transform.rotation.z);
+	}
+	
+	void Update ()
+	{
+		transform.position = new Vector3(
+			_player.transform.position.x,
+			_player.transform.position.y + 10,
+			_player.transform.position.z);
+
+		transform.rotation = Quaternion.Slerp(
+			transform.rotation,
+			Quaternion.Euler(90,
+				_player.transform.rotation.y,
+				_player.transform.rotation.z),
+			Time.deltaTime * RotateFollowSmoothing);
+	}
+}
