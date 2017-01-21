ced760335afdd606d1bedd63bc8c53db048034ff
Scott Richmond <s.t.richmond@gmail.com>
2017-01-21 11:15:27 +0000

    Player now knows how deep the water is.

---
+++ Wacky Waves/Assets/Scripts/WaterTypeResolver.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterTypeResolver : MonoBehaviour
+{
+	private Terrain _currentTerrain;
+	public float CurrentTerrainHeight;
+
+	void Start () {
+		var ray = new Ray(transform.position, transform.TransformDirection(Vector3.down));
+		RaycastHit hit;
+		if (Physics.Raycast(ray, out hit, 50, LayerMask.GetMask("Ground")))
+		{
+			_currentTerrain = hit.transform.GetComponent<Terrain>();
+		}
+		else
+		{
+			Debug.LogError("Fuck me sideways I could find a terrain under me!");
+		}
+	}
+	
+	void Update ()
+	{
+		CurrentTerrainHeight = _currentTerrain.terrainData.GetHeight((int) transform.position.x, (int) transform.position.z);
+	}
+}
