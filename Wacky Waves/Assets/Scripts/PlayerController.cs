9a4ac5a0803bd64eb3fbb8d2922714bad3318c15
Dave Cooper <dave.e.cooper@gmail.com>
2017-01-21 11:37:06 +0000

    Modified Player Controller for full rotation Added Dave Scene,

--- Wacky Waves/Assets/Scripts/PlayerController.cs
+++ Wacky Waves/Assets/Scripts/PlayerController.cs
@@ -8,7 +8,7 @@ public class PlayerController : MonoBehaviour
 	public float ForwardSpeed = 1.0f;
 	public float TurnSpeed = 30.0f;
 	public float AngleSmoothing = 2.0f;
-
+    private float direction;
 	void Start ()
 	{
 
@@ -16,9 +16,9 @@ public class PlayerController : MonoBehaviour
 	
 	void Update ()
 	{
-		transform.Translate((Vector3.forward * ForwardSpeed), Space.Self);
-
-		Quaternion target = Quaternion.Euler(0, Input.GetAxisRaw("Horizontal") * TurnSpeed, 0);
+		transform.Translate(Vector3.forward* ForwardSpeed, Space.Self);
+        direction += Input.GetAxisRaw("Horizontal");
+		Quaternion target = Quaternion.Euler(0, direction * TurnSpeed, 0);
 		transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * AngleSmoothing);
 
 	}
