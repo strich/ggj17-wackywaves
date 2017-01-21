638427a9a3bf066cbb17eb1fd9dd231d38a29c6c
Dave Cooper <dave.e.cooper@gmail.com>
2017-01-21 17:43:13 +0000

    Added Score HUD (UI ScoreUpdater) Added ScoreManager - Use me to increment score and receive notifications

---
+++ Wacky Waves/Assets/ScoreManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour {
+
+    public static readonly string WavePoints = "Wave";
+    public static readonly string DolphinPoints = "Dolphin";
+    public static readonly string SurferPoints = "Surfer";
+    public static readonly string TrashMonster = "TrashMonster";
+    public static readonly string Shark = "Shark";
+    public static readonly string Tree = "Tree";
+    public static readonly string Boat = "Boat";
+    public static readonly string Clicked = "Clicked";
+
+    private static ScoreManager instance;
+    public static ScoreManager Instance { get { return instance; } private set { instance = value; } }
+    
+    public delegate void ScoreHandler(int points, string tag);
+    public event ScoreHandler ScoreHandlers;
+
+    private int score;
+    
+    private Dictionary<string, int> scores;
+
+    void Awake ()
+    {
+        instance = this;
+        scores = new Dictionary<string, int>();
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void UpdateScore(int points, string tag)
+    {
+        int dscore=0;
+        score += points;
+        if (!scores.TryGetValue(tag, out dscore))
+        {
+            scores.Add(tag, points);
+        }
+        else
+        {
+            scores[tag] = points + dscore;
+        }
+
+        if (ScoreHandlers!=null)
+        {
+            ScoreHandlers.Invoke(points, tag);
+        }
+    }
+
+    public bool  GetSubScore (string tag, out int score)
+    {
+        return scores.TryGetValue(tag, out score);
+    }
+   
+
+
+
+}
---
+++ Wacky Waves/Assets/ScoreUpdater.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreUpdater : MonoBehaviour {
+
+  
+
+
+    private int score = 0;
+    public int Score { get { return score; } private set { score = value; } }
+
+    private Text scoreText;
+    void Awake()
+    {
+        scoreText = GetComponent<Text>();
+    }
+
+    // Use this for initialization
+    void Start() {
+        ScoreManager.Instance.ScoreHandlers += UpdateScore;
+        // get instance of  ScoreManager
+        // register our delegate as a handler for score update
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    void UpdateScore(int points, string tag)
+    {
+        score += points;
+        scoreText.text = score.ToString();
+        Debug.Log(tag + " scores " + points + " points");
+    }
+
+    void OnDestroy()
+    {
+        ScoreManager.Instance.ScoreHandlers -= UpdateScore;
+    }
+
+    public void DelegateScoreUpdate(int points)
+    {
+        ScoreManager.Instance.UpdateScore(points, ScoreManager.Clicked);
+    }
+
+}
