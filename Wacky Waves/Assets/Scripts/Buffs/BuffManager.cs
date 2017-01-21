4d5c801666ae8f5caad4b93d3e7c41f7d1d230cb
Chris McMath <chrismcmath@gmail.com>
2017-01-21 16:26:47 +0000

    Buff manager, decreasing buff, Potential to speed logic

---
+++ Wacky Waves/Assets/Scripts/Buffs/BuffManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuffManager : MonoBehaviour
+{
+    public const string KEY_SPEED = "KEY_SPEED";
+
+    public Dictionary<string, List<Buff>> _Buffs = new Dictionary<string, List<Buff>>();
+
+    public void AddBuff(string key, Buff buff)
+    {
+        if (!_Buffs.ContainsKey(key))
+        {
+            _Buffs.Add(key, new List<Buff>());
+        }
+
+        _Buffs[key].Add(buff);
+    }
+
+    public float Modify(string key, float value)
+    {
+        if (!_Buffs.ContainsKey(key))
+        {
+            Debug.LogErrorFormat("No buff with key {0}", key);
+            return 1f;
+        }
+        else
+        {
+            return Modify(_Buffs[key], value);
+        }
+    }
+
+    public void Wipe(string key)
+    {
+        if (!_Buffs.ContainsKey(key))
+        {
+            Debug.LogErrorFormat("No buff with key {0}", key);
+        }
+        else
+        {
+            _Buffs[key].Clear();
+        }
+    }
+
+    float Modify(List<Buff> buffs, float value)
+    {
+        for (int i = 0; i < buffs.Count; ++i)
+        {
+            value = buffs[i].Modify(value);
+        }
+        return value;
+    }
+}
--- Wacky Waves/Assets/Scripts/PlayerController.cs
+++ Wacky Waves/Assets/Scripts/PlayerController.cs
@@ -1,21 +1,86 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-	public float ForwardSpeed = 1.0f;
+	public float ForwardSpeedModifier = 1.0f;
+
 	public float TurnSpeed = 30.0f;
 	public float AngleSmoothing = 2.0f;
+	public float PotentialDropOff = 2f;
 
     [SerializeField]
     float _Rotation;
 
-    public Quaternion _RotationTarget = Quaternion.identity;
+    //NOTE: Testing vars
+    [SerializeField]
+    float _Speed;
+    [SerializeField]
+    float _Potential; //range 0 to 1
 
-	void Update ()
+    BuffManager _BuffManager;
+    WaterTypeResolver _WaterTypeResolver;
+    WaterType _CurrentWaterType;
+
+    void Awake()
+    {
+        _BuffManager = gameObject.AddComponent<BuffManager>();
+        _WaterTypeResolver = GetComponent<WaterTypeResolver>();
+        _WaterTypeResolver.OnWaterTypeChanged += OnWaterTypeChanged;
+    }
+
+    void Update ()
 	{
         ApplyInput();
         Move();
 	}
+
+    void OnDestroy()
+    {
+        _WaterTypeResolver.OnWaterTypeChanged -= OnWaterTypeChanged;
+    }
+
+    void OnWaterTypeChanged(WaterType waterType)
+    {
+        _BuffManager.Wipe(BuffManager.KEY_SPEED);
+
+        switch (waterType)
+        {
+            case WaterType.Deep:
+                OnDeep();
+                break;
+            case WaterType.Shallow:
+                OnShallow();
+                break;
+            case WaterType.Ground:
+                OnGround();
+                break;
+        }
+
+        _CurrentWaterType = waterType;
+    }
+
+    void OnGround()
+    {
+    }
+
+    void OnShallow()
+    {
+        if (_CurrentWaterType == WaterType.Deep)
+        {
+            float deviation = GameUtils.GetDeviation(transform);
+            float potential = Mathf.Pow(((90f - deviation) / 90f), PotentialDropOff);
+            Debug.LogFormat("[Player] From deviation {0} got potential {1}", deviation, potential);
+            SetPotential(potential);
+        }
+    }
+
+    void OnDeep()
+    {
+        _BuffManager.AddBuff(BuffManager.KEY_SPEED, new DecreasingBuff(_Potential * 15f, 0.8f));
+        SetPotential(0f);
+        UpdateView();
+    }
 
     void ApplyInput()
     {
@@ -29,9 +94,25 @@ public class PlayerController : MonoBehaviour
 
     void Move()
     {
-		transform.Translate(Vector3.forward* ForwardSpeed, Space.Self);
+		transform.Translate(Vector3.forward * GetForwardSpeed(), Space.Self);
 		transform.rotation = Quaternion.Slerp(transform.rotation,
             Quaternion.Euler(0f, _Rotation, 0f),
             Time.deltaTime * AngleSmoothing);
+    }
+
+    void SetPotential(float power)
+    {
+        _Potential = Mathf.Clamp01(power);
+        UpdateView();
+    }
+
+    void UpdateView()
+    {
+        transform.localScale = Vector3.one + Vector3.one * _Potential;
+    }
+
+    float GetForwardSpeed()
+    {
+        return ForwardSpeedModifier * _BuffManager.Modify(BuffManager.KEY_SPEED, GameUtils.GetTerrainSpeed(_CurrentWaterType));
     }
 }
