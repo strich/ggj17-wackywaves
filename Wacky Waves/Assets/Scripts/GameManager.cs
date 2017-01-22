7c2e9d4fbaaa4f3f88ccdccb9bef93b45956954f
Chris McMath <chrismcmath@gmail.com>
2017-01-22 03:41:32 +0000

    Game manager stub

---
+++ Wacky Waves/Assets/Scripts/GameManager.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public enum State { TITLE, LIVE, GAME_OVER}
+
+    public delegate void StateHandler(State state);
+    public static event StateHandler OnStateChanged;
+
+    State _State = State.TITLE;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        SceneManager.LoadScene("MainMenu");
+    }
+
+	void Update()
+    {
+        switch (_State)
+        {
+            case State.TITLE:
+                UpdateTitle();
+                break;
+            case State.LIVE:
+                UpdateLive();
+                break;
+            case State.GAME_OVER:
+                UpdateGameOver();
+                break;
+        }
+	}
+
+    void ChangeState(State state)
+    {
+        _State = state;
+
+        switch (_State)
+        {
+            case State.TITLE:
+                break;
+            case State.LIVE:
+                SceneManager.LoadScene("Chris Scene");
+                break;
+            case State.GAME_OVER:
+                UpdateGameOver();
+                break;
+        }
+
+        if (OnStateChanged != null)
+        {
+            OnStateChanged(_State);
+        }
+    }
+
+    void UpdateTitle()
+    {
+        if (Input.GetAxis("Horizontal") != 0f)
+        {
+            ChangeState(State.LIVE);
+        }
+    }
+
+    void UpdateLive()
+    {
+    }
+
+    void UpdateGameOver()
+    {
+    }
+}
---
+++ Wacky Waves/Assets/Scripts/UIController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour {
+    public GameObject Title;
+
+	void Start () {
+        GameManager.OnStateChanged += OnStateChanged;
+	}
+
+    void OnDestroy()
+    {
+    }
+
+    void OnStateChanged(GameManager.State state)
+    {
+        switch (state)
+        {
+            case GameManager.State.TITLE:
+                Title.SetActive(true);
+                break;
+            case GameManager.State.LIVE:
+                Title.SetActive(false);
+                break;
+            case GameManager.State.GAME_OVER:
+                Title.SetActive(false);
+                break;
+        }
+    }
+}
