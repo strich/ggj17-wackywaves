115fa31fbddecca6ce58155542b26f92134209b1
Dave Cooper <dave.e.cooper@gmail.com>
2017-01-22 03:23:21 +0000

    Combo Manager Getting there GrindHandler Underway

---
+++ Wacky Waves/Assets/Scripts/ComboHandlers/GrindHandler.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrindHandler : MonoBehaviour {
+    public const string GRIND_START = "GRIND_START";
+    public const string GRIND_OVER  = "GRIND_OVER";
+
+    private static GrindHandler instance;
+    public static GrindHandler Instance
+    {
+        get
+        {
+            if (!instance)
+            {
+                instance = new GameObject("GrindHandler").AddComponent<GrindHandler>();
+            }
+            return instance;
+        }
+        private set { instance = value; }
+    }
+
+    // Use this for initialization
+    void Start () {
+        ComboManager.Instance.ComboHandlers += HandleGrind;
+        ComboManager.ComboElement GrindCombo = new ComboManager.ComboElement();
+        GrindCombo.Name = "Grind";
+        GrindCombo.Instant = false;
+        GrindCombo.Points = 1;
+        ComboManager.Instance.CreateComboElement(GrindCombo);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		// Display Points for every deltatime in grind
+	}
+
+    bool inGrind;
+
+    void HandleGrind(string tag, int quantity)
+    {
+        switch (tag)
+        {
+            case GRIND_START:
+                    Debug.Log(GRIND_START);
+                if (!inGrind)
+                {
+                    inGrind = true;
+                }
+                else
+                {
+                    Debug.LogWarning("already in grind!");
+                }
+                break;
+            case GRIND_OVER:
+                Debug.Log(GRIND_OVER);
+                // end grind
+                // Add a GrindCombo in Combo Manager 
+                break;
+            default:
+                Debug.Log("Not for Grinds Handler");
+                break;
+
+        }
+    }
+}
--- Wacky Waves/Assets/Scripts/ComboManager.cs
+++ Wacky Waves/Assets/Scripts/ComboManager.cs
@@ -8,15 +8,19 @@ public class ComboManager : MonoBehaviour {
     // Tags
     public static readonly string MULTIPLIER = "MULTIPLIER";
     public static readonly string POINTS = "POINTS";
-   //  public static readonly string SPEEDUP = "SPEEDUP"; ?
+    public static readonly string COMBO_OVER = "COMBO_OVER";
+    //  public static readonly string SPEEDUP = "SPEEDUP"; ?
 
-    public delegate void ComboHandler(int value, string tag);
-    public event ComboHandler ScoreHandlers;
+
+    public delegate void ComboHandler(string tag, int quantity);
+    public event ComboHandler ComboHandlers;
 
     public float ComboCountdown;
     private float countdown;
      
     private Dictionary<string, int> CurrentCombo;
+    private int comboScore;
+    private int comboMultiplier;
     private Dictionary<string, ComboElement> ComboElements;
 
 
@@ -54,8 +58,6 @@ public class ComboManager : MonoBehaviour {
     {
         ComboElements = new Dictionary<string, ComboElement>();
         CurrentCombo = new Dictionary<string, int>();
-
-
     }
     // Use this for initialization
     void Start () {
@@ -64,8 +66,31 @@ public class ComboManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (countdown>0)
+        {
+            countdown -= Time.deltaTime;
+            if (countdown <= 0)
+            {
+                countdown = 0;
+                // Combo Over!
+
+                // Send Points to ScoreManager
+                ScoreManager.Instance.UpdateScore(comboMultiplier * comboScore, COMBO_OVER);
+                // Clear Combos
+                ResetCombo();
+                
+
+            }
+        }
 	}
+
+    void ResetCombo()
+    {
+        ComboHandlers.Invoke(COMBO_OVER, 0);
+        CurrentCombo    = new Dictionary<string, int> ();
+        comboScore      = 0;
+        comboMultiplier = 0;
+    }
 
 
 
@@ -73,19 +98,40 @@ public class ComboManager : MonoBehaviour {
     /// Add a comboelement to current combo
     /// </summary>
     /// <param name="element"></param>
-    public void AddComboElement(string element)
+    public void AddComboElement(string element, bool end)
     {
         int count = 0;
-        if (CurrentCombo.TryGetValue(element, out count))
+        ComboElement elementT;
+        if (!ComboElements.TryGetValue(element, out elementT))
+        {
+            Debug.LogError("ComboElement " + element + " does not exist!");
+        }
+
+        bool inCombo = CurrentCombo.TryGetValue(element, out count);
+
+       // int increment;
+        //if (elementT.Instant)
+        //{
+        //    increment = 1;
+        //}
+        // else a registered combo handler 
+        // invocation will deal with it;
+       
+        if (inCombo)
         {
             CurrentCombo[element] = count + 1;
         }
         else
         {
             CurrentCombo.Add(element, 1);
+            count = 1;
         }
+
         countdown = ComboCountdown;
-       //heckCombos();
+        //heckCombos();
+        // 
+        // deal with any continous or other odd combo rules
+        ComboHandlers.Invoke(element, count);
     }
 
     ///// <summary>
@@ -108,7 +154,7 @@ public class ComboManager : MonoBehaviour {
         }
         else
         {
-            Debug.LogWarning("ComboElement " + element.Name + " already exisists!");
+            Debug.LogWarning("ComboElement " + element.Name + " already exists!");
         }
     }
 }
